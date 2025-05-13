ffd9918d53e760362c3d59980eb2f3a7b32bdbf2
Borges dev <leosull50@hotmail.com>
2025-05-13 13:15:46 -0300

    feat: add summary cards

---
+++ FSW/finance-ai/app/(home)/page.tsx
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time_select";
+import { isMatch } from "date-fns";
+import { getDashboard } from "../_data/get-dashboard";
+
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+  if (monthIsInvalid) {
+    redirect(`?month=${new Date().getMonth() + 1}`);
+  }
+  const dashboard = await getDashboard(month);
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <TimeSelect />
+      </div>
+      <SummaryCards month={month} {...dashboard} />
+    </>
+  );
+};
+
+export default Home;
--- FSW/finance-ai/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import Navbar from "../_components/navbar";
-
-const NavBar = () => {
-  return <Navbar />;
-};
-
-export default NavBar;
